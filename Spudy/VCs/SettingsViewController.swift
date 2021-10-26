f52beb25c8d0225dbcc939079289084fb0ecd3d1
lillytian <lillytian2001@gmail.com>
2021-10-26 16:33:25 -0500

    added alerts for settings

--- Spudy/VCs/SettingsViewController.swift
+++ Spudy/VCs/SettingsViewController.swift
@@ -9,25 +9,54 @@ import UIKit
 import FirebaseAuth
 
 class SettingsViewController: UIViewController {
-
+    
+    @IBOutlet weak var selfStudyModeSwitch: UISwitch!
+    var user: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // TODO: CHANGE WITH BACKEND
+        selfStudyModeSwitch.isOn = user
         // Do any additional setup after loading the view.
     }
     
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func changeSelfStudyMode(_ sender: Any) {
+        user = selfStudyModeSwitch.isOn
     }
-    */
+    
+    @IBAction func touchChangeNotifSettings(_ sender: Any) {
+        let controller = UIAlertController(title: "Change notification settings", message: nil, preferredStyle: .actionSheet)
+        
+        controller.addAction(UIAlertAction(title: "Send me all notifications", style: .default, handler: nil))
+        controller.addAction(UIAlertAction(title: "Notify me when friends are near", style: .default, handler: nil))
+        controller.addAction(UIAlertAction(title: "Don't send me notifications", style: .default, handler: nil))
+        controller.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func touchChangeLocationSettings(_ sender: Any) {
+        let controller = UIAlertController(title: "Share my location with:", message: nil, preferredStyle: .actionSheet)
+        
+        controller.addAction(UIAlertAction(title: "Everyone", style: .default, handler: nil))
+        controller.addAction(UIAlertAction(title: "Just friends", style: .default, handler: nil))
+        controller.addAction(UIAlertAction(title: "Don't share my location", style: .default, handler: nil))
+        controller.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(controller, animated: true, completion: nil)
+    }
+    
     @IBAction func touchSignOut(_ sender: Any) {
+        let controller = UIAlertController(title: "Sign Out", message: "Are you sure you want to sign out of your account?", preferredStyle: .alert)
+        
+        controller.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        controller.addAction(UIAlertAction(title: "Sign Out", style: .default, handler: {(action) in self.signUserOut()}))
+
+        present(controller, animated: true, completion: nil)
+    }
+    
+    func signUserOut() {
         do {
             try Auth.auth().signOut()
             self.performSegue(withIdentifier: Constants.Segues.logoutSegueIdentifier, sender: nil)
@@ -41,7 +70,18 @@ class SettingsViewController: UIViewController {
             alert.addAction(UIAlertAction(title: Constants.Messages.ok, style:.default))
             self.present(alert, animated: true, completion: nil)
         }
-
     }
     
+    @IBAction func touchDeleteAccount(_ sender: Any) {
+        let controller = UIAlertController(title: "Delete Account", message: "Are you sure you want to delete your account?", preferredStyle: .alert)
+        
+        controller.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        controller.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: {(action) in self.deleteUser()}))
+        
+        present(controller, animated: true, completion: nil)
+    }
+    
+    func deleteUser() {
+        print("will delete user...")
+    }
 }
