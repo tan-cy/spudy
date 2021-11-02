f0db7f27c2ef532100658bd8ebb582d770ecf22d
Shamira Kabir <shamirakabir@utexas.edu>
2021-11-02 17:30:25 -0500

    Change the UI of spudy spots page

---
+++ Spudy/VCs/StudySpotsCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  StudySpotsCollectionViewCell.swift
+//  Spudy
+//
+//  Created by Shamira Kabir on 10/28/21.
+//
+
+import UIKit
+
+class StudySpotsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+}
