a0ecb67686be3a099d17a46f372301f06ba7d3b1
lillytian <lillytian2001@gmail.com>
2021-11-18 18:10:54 -0600

    google login auth

--- Spudy/VCs/LoginViewController.swift
+++ Spudy/VCs/LoginViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import GoogleSignIn
 import FirebaseDatabase
 import CoreData
 
@@ -15,16 +16,20 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var googleSignInButton: GIDSignInButton!
     
+    var ref: DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         emailTextField.placeholder = Constants.Placeholders.email
         passwordTextField.placeholder = Constants.Placeholders.password
         
         passwordTextField.isSecureTextEntry = true
+        
+        ref = Database.database().reference(withPath: Constants.DatabaseKeys.profilePath)
 
         clearCoreData()
         Auth.auth().addStateDidChangeListener() { auth, user in
@@ -47,12 +52,14 @@ class LoginViewController: UIViewController {
                           newSignedIn.setValue(username, forKey: Constants.CoreKeys.username)
                           
                           CURRENT_USERNAME = username as! String
+                          print("current user is " + CURRENT_USERNAME)
                           
                           // fetch current users
                           let fetchedResults = self.retrieveCurrentUser()
 
                           // if no user exists
                           if (fetchedResults.isEmpty) {
+                              print("no user exists")
                               let newSignedIn = NSEntityDescription.insertNewObject(forEntityName: Constants.CoreKeys.userEntity, into: Constants.context)
                               newSignedIn.setValue(username, forKey: Constants.CoreKeys.username)
                           } else {
@@ -135,6 +142,93 @@ class LoginViewController: UIViewController {
               self.present(alert, animated: true, completion: nil)
           }
         }
+    }
+    
+    @IBAction func touchGoogleLogin(_ sender: Any) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [unowned self] user, error in
+
+            if error != nil {
+                let errorAlert = UIAlertController(title: "Unable to Login", message: "We were not able to login using your Google Account.", preferredStyle: .alert)
+                errorAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                present(errorAlert, animated: true, completion: nil)
+                return
+            }
+
+            guard
+                let authentication = user?.authentication,
+                let idToken = authentication.idToken
+            else {
+                return
+            }
+
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: authentication.accessToken)
+            googleSignIn(credential: credential)
+        
+        }
+    }
+    
+    func googleSignIn(credential: AuthCredential) {
+        Auth.auth().signIn(with: credential) { authResult, error in
+            if error != nil {
+                let errorAlert = UIAlertController(title: "Unable to Login", message: "We were not able to login using your Google Account.", preferredStyle: .alert)
+                errorAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(errorAlert, animated: true, completion: nil)
+            } else {
+                // check if this is a new user logging in w google
+                guard let newUserStatus = authResult?.additionalUserInfo?.isNewUser else { return }
+                    
+                if newUserStatus {
+                    // time to make their username and store it in firebase!
+                    let profileData = authResult?.additionalUserInfo?.profile as! [String: Any]
+                    
+                    let name = profileData["name"] as? String ?? "Unknown"
+                    let email = profileData["email"] as! String
+                    
+                    // create username using the email
+                    let endIndex = email.firstIndex(of: "@")
+                    let username = email.prefix(email.distance(from: email.startIndex, to: endIndex!))
+                    
+                    var reformatUsername = ""
+                    username.forEach { currChar in
+                        if currChar == "." {
+                            reformatUsername.append("_")
+                        } else if !"#$[]".contains(currChar) {
+                            reformatUsername.append(currChar)
+                        }
+                    }
+                    
+                    self.initializeUserData(username: String(reformatUsername), email: email, name: name)
+                }
+            }
+        }
+    }
+    
+    func initializeUserData(username: String, email: String, name: String) {
+        let newItemRef = self.ref.child(username)
+        newItemRef.child(Constants.DatabaseKeys.email).setValue(email)
+        newItemRef.child(Constants.DatabaseKeys.name).setValue(name)
+        
+        // profile data
+        newItemRef.child(Constants.DatabaseKeys.classes).setValue([])
+        newItemRef.child(Constants.DatabaseKeys.contactInfo).setValue("")
+        newItemRef.child(Constants.DatabaseKeys.gradYear).setValue("")
+        newItemRef.child(Constants.DatabaseKeys.major).setValue("")
+        newItemRef.child(Constants.DatabaseKeys.photo).setValue("")
+        
+        // settings data
+        let settingsRef = newItemRef.child(Constants.DatabaseKeys.settings)
+        let settingsData: [String : Any] = [
+            Constants.DatabaseKeys.selfStudy: false,
+            Constants.DatabaseKeys.notificationSetting: Constants.LocationSettings.everyone.rawValue,
+            Constants.DatabaseKeys.locationSetting: Constants.LocationSettings.everyone.rawValue
+        ]
+        settingsRef.setValue(settingsData)
     }
     
 }
