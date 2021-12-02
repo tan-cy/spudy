0e915ea084f7413fea62e0c6963bc8fefae55f67
lillytian <lillytian2001@gmail.com>
2021-12-02 01:56:17 -0600

    search bar changes

--- Spudy/VCs/SearchViewController.swift
+++ Spudy/VCs/SearchViewController.swift
@@ -6,24 +6,62 @@
 //
 
 import UIKit
+import simd
 
-class SearchViewController: UIViewController {
-
+class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    @IBOutlet weak var buildingsSearchBar: UISearchBar!
+    @IBOutlet weak var buildingsTableView: UITableView!
+    
+    let tableViewCellIdentifier = "searchTableViewCell"
+    let segueIdentifier = "searchToStudySpotSegueIdentifier"
+    
+    var filteredData: [building]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        buildingsTableView.delegate = self
+        buildingsTableView.dataSource = self
+        buildingsSearchBar.delegate = self
+        filteredData = buildings
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(filteredData.count)
+        return filteredData.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = buildingsTableView.dequeueReusableCell(withIdentifier: tableViewCellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = filteredData[indexPath.row].name
+        return cell
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredData = searchText.isEmpty ? buildings : buildings.filter { (item) in
+            
+            return item.name.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        buildingsTableView.reloadData()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == segueIdentifier,
+           let destination = segue.destination as? StudySpotViewController,
+           let filteredIndex = buildingsTableView.indexPathForSelectedRow?.row {
+            
+            let building = filteredData[filteredIndex]
+            let actualIndex = buildings.firstIndex(where: { (currBuilding) in
+               return building.name == currBuilding.name
+            })
+        
+            destination.index = actualIndex ?? 0
+        }
+    
+    }
 
 }
